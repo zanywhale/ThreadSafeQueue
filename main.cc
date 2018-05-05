c49329171c9f70cac0f567e71b8813defd060d73
zanywhale <zanywhale@gmail.com>
2018-05-05 13:54:37 +0900

    feat: Add safequeue test code

--- main.cc
+++ main.cc
@@ -2,7 +2,13 @@
 
 int main(int argc, char *argv[]){
 
-  SafeQueue::SafeQueue<std::string> sQ();
+  SafeQueue::SafeQueue<std::string> sQ;
+  std::string a{"Test SafeQueue!!"};
+  std::cout << sQ.Empty() << std::endl;
+  sQ.Push(a);
+  std::cout << sQ.Empty() << std::endl;
+  std::cout << sQ.Pop() << std::endl;
+  std::cout << sQ.Empty() << std::endl;
 
   return 0;
 }
